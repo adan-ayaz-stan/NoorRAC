62b7af9fd87322e1dba1011e220af2474d26a9af
Adan Ayaz <adanayaztracer@gmail.com>
2025-04-08 00:04:45 +0500

    Coagulation of team member's work

---
+++ Views/Cars.xaml.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Runtime.ConstrainedExecution;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+public class Car : INotifyPropertyChanged
+{
+    private string _status = "Available for rent";
+    private string _statusBackground = "#141F15"; // Default background for status
+    private string _statusForeground = "#7ED4AD"; // Default foreground for status
+    private string _licensePlate = "AJK-838";
+    private string _modelInfo = "Toyota Corolla 2011";
+    private string _rentPerDay = "Rs. 3500";
+    private string _totalRentals = "258";
+    private string _avgRentalIncome = "Rs. 30,000";
+
+    public string Status
+    {
+        get => _status;
+        set => SetProperty(ref _status, value);
+    }
+
+    public string StatusBackground // Bind Border Background to this
+    {
+        get => _statusBackground;
+        set => SetProperty(ref _statusBackground, value);
+    }
+    public string StatusForeground // Bind TextBlock Foreground to this
+    {
+        get => _statusForeground;
+        set => SetProperty(ref _statusForeground, value);
+    }
+
+    public string LicensePlate
+    {
+        get => _licensePlate;
+        set => SetProperty(ref _licensePlate, value);
+    }
+
+    public string ModelInfo
+    {
+        get => _modelInfo;
+        set => SetProperty(ref _modelInfo, value);
+    }
+
+    public string RentPerDay
+    {
+        get => _rentPerDay;
+        set => SetProperty(ref _rentPerDay, value);
+    }
+
+    public string TotalRentals
+    {
+        get => _totalRentals;
+        set => SetProperty(ref _totalRentals, value);
+    }
+
+    public string AvgRentalIncome
+    {
+        get => _avgRentalIncome;
+        set => SetProperty(ref _avgRentalIncome, value);
+    }
+
+    // --- INotifyPropertyChanged Implementation ---
+    public event PropertyChangedEventHandler PropertyChanged;
+
+    protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    protected bool SetProperty<T>(ref T storage, T value, [CallerMemberName] string propertyName = null)
+    {
+        if (EqualityComparer<T>.Default.Equals(storage, value)) return false;
+        storage = value;
+        OnPropertyChanged(propertyName);
+        return true;
+    }
+    // --- End INotifyPropertyChanged ---
+
+    // You would also add Commands here if using MVVM for the buttons
+    // public ICommand EditCommand { get; }
+    // public ICommand SeeMoreCommand { get; }
+}
+
+namespace NoorRAC.Views
+{
+    /// <summary>
+    /// Interaction logic for Cars.xaml
+    /// </summary>
+    public partial class Cars : UserControl
+    {
+        public ObservableCollection<Car> CarList { get; set; } = new ObservableCollection<Car>();
+
+        public Cars()
+        {
+            InitializeComponent();
+            LoadSampleCars(5); // Load 5 cars for the example
+
+            // Set the DataContext if not already set (crucial for Binding)
+            // If using code-behind:
+            this.DataContext = this;
+        }
+
+        private void LoadSampleCars(int count)
+        {
+            for (int i = 1; i <= count; i++)
+            {
+                // Create slightly different data for each car
+                CarList.Add(new Car
+                {
+                    Status = (i % 2 == 0) ? "Rented" : "Available for rent",
+                    StatusBackground = (i % 2 == 0) ? "#F6DC43" : "#141F15",
+                    StatusForeground = (i % 2 == 0) ? "Black" : "#7ED4AD",
+                    LicensePlate = $"XYZ-{100 + i}",
+                    ModelInfo = $"Car Model {2010 + i}",
+                    RentPerDay = $"Rs. {3000 + (i * 100)}",
+                    TotalRentals = $"{100 + (i * 20)}",
+                    AvgRentalIncome = $"Rs. {25000 + (i * 1000)}"
+                });
+            }
+        }
+    }
+}
---
+++ Views/EditCustomer.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace NoorRAC.Views
+{
+    /// <summary>
+    /// Interaction logic for EditCustomer.xaml
+    /// </summary>
+    public partial class EditCustomer : UserControl
+    {
+        public ObservableCollection<RentalRecord> RentalRecords { get; set; }
+        public EditCustomer()
+        {
+            InitializeComponent();
+            RentalRecords = new ObservableCollection<RentalRecord>
+            {
+                new RentalRecord { ID = 1, ClientName = "Toyota Corolla", CarType = "AB123CD", CarNumber = "$50", Status = "Rented", RentalDate = "2025-04-01" },
+                new RentalRecord { ID = 2, ClientName = "Honda Civic", CarType = "XY987ZT", CarNumber = "$60", Status = "Available", RentalDate = "2025-03-28" },
+                new RentalRecord { ID = 3, ClientName = "Ford Focus", CarType = "LM456OP", CarNumber = "$55", Status = "Rented", RentalDate = "2025-03-25" },
+                new RentalRecord { ID = 4, ClientName = "Chevrolet Malibu", CarType = "GH654RT", CarNumber = "$65", Status = "Available", RentalDate = "2025-03-20" },
+                new RentalRecord { ID = 5, ClientName = "Nissan Altima", CarType = "UV789WX", CarNumber = "$58", Status = "Rented", RentalDate = "2025-03-15" }
+
+            };
+            DataContext = this;
+        }
+    }
+    public class RentalRecord
+    {
+        public int ID { get; set; }
+        public string ClientName { get; set; }
+        public string CarType { get; set; }
+        public string CarNumber { get; set; }
+        public string Status { get; set; }
+        public string RentalDate { get; set; }
+    }
+}
